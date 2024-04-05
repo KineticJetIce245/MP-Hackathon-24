76e7066c55196cba291484591f1b449a5e4a18b7
KineticJetIce245 <kineticjetice245@gmail.com>
2024-04-05 19:15:56 -0400

    add point loop

---
+++ PointLoop.py
@@ -0,0 +1,4 @@
+from Point import Point
+class PointLoop:
+    def __init__(self, loop: list[Point]) -> None:
+        self.point_list = loop
--- PointStage.py
+++ PointStage.py
@@ -1,4 +1,5 @@
 from Point import Point
+from PointLoop import PointLoop
 
 # Point Clouds
 class PointStage:
@@ -6,4 +7,9 @@ class PointStage:
     def __init__(self, point_cloud: list[Point]) -> None:
         self.point_cloud = point_cloud
         
+    def loop_dify(self) -> list[PointLoop]:
+        pass
+    
+    def 
+   
 s = PointStage()
