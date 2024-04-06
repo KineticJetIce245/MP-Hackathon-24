874a4fa41e792d02c3f4d8bfc2acd1faa61f1a94
KineticJetIce245 <kineticjetice245@gmail.com>
2024-04-06 00:34:55 -0400

    Merge Files to PointCloud

--- PointCloud.py
+++ PointCloud.py
@@ -1,6 +1,50 @@
-from Point import Point
-from PointLoop import PointLoop
-import random
+import math
+
+#class with coordinate attributes , initialize with coordinates,
+#give coordinate (function that allows to give code)
+#put it into one dictionnary
+class Point:
+    def __init__(self, coordinate: list[float]) -> None:
+        self.coordinates = {"x": coordinate[0], "y": coordinate[1], "z": coordinate[2]}
+        self.coordinates.update(self.__calculate_sph_coordinate())
+
+    def __calculate_sph_coordinate(self) -> dict:
+        x = self.coordinates["x"]
+        y = self.coordinates["y"]
+        z = self.coordinates["z"]
+        r = math.sqrt(x**2 + y**2 + z**2)
+        r2d = math.sqrt(x**2 + y**2)
+        theta = math.atan2(y,x)
+        if z == 0:
+            phi = 0
+        elif z > 0:
+            if r2d == 0:
+                phi = math.pi/2
+            else:
+                phi = math.atan(z/r2d)
+        else:
+            phi = math.atan(z/r2d)
+        return {'r': r, 'theta': theta, 'phi': phi}
+    
+    def __str__(self) -> str:
+        return str(self.coordinates["x"]) + "," + str(self.coordinates["y"]) + "," + str(self.coordinates["z"])
+
+
+class PointLoop:
+    def __init__(self, loop: list[Point]) -> None:
+        self.point_list = loop
+        self.__close()
+           
+    def __str__(self) -> str:
+        ret_val = "["
+        for k in self.point_list:
+            ret_val += k.__str__() + ";"
+        return ret_val
+
+    # Close up the loops
+    def __close(self) -> None:
+        self.point_list.sort(key = lambda a: (a.coordinates["theta"], a.coordinates["r"]))
+        
 # Point Clouds
 class PointStage:
     # Initiate with an array of points
@@ -68,19 +112,3 @@ class PointStage:
         loops.append(PointLoop(loop))
         loops.append(PointLoop(last_loop))
         return (loops)
-
-test_list = [
-    Point([-1, 3, 4]),
-    Point([-1, 2, 8]),
-    Point([0, 3, 8]),
-    Point([1, 2, 0]),
-    Point([3, 5, 2]),
-    Point([2, 2, 5]),
-    Point([4, 2, 4]),
-    Point([3, 7, 4]),
-    Point([4, 3, 4]),
-]      
-s = PointStage(test_list)
-for i in s.loopdify():
-    print(i)
-    
--- test.py
+++ test.py
@@ -1,5 +1,2 @@
-from Point import Point
-a = [Point([1, 1, 0]), Point([1, 2, 0]), Point([1, 5, 0]), Point([1, 9, 0])]
-a.sort(key = lambda a: a.coordinates["x"])
-for i in a:
-    print(i)
+from PointCloud import *
+
